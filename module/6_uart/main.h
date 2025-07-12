e59e182ed468ac032079a5c414b3155dcfb4091d
LJ951 <124690250+LJ951@users.noreply.github.com>
2025-07-12 11:16:15 +0800

    Add files via upload

---
+++ module/6_uart/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include "clock.h"
+#include "OLED.h"
+#include "Serial.h"
+#include "mpu6050.h"
+#include "stdio.h"
+#include "ti_msp_dl_config.h"
+
+#endif  /* #ifndef _MAIN_H_ */
