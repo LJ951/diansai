9a2e2824c5a500da7047c1169d0e8aefd3f1ea54
LJ951 <124690250+LJ951@users.noreply.github.com>
2025-07-12 11:00:59 +0800

    Delete 8_PID directory

--- 8_PID/Debug/Drivers/OLED/OLED_Data.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-Drivers/OLED/OLED_Data.o: ../Drivers/OLED/OLED_Data.c \
- ../Drivers/OLED/OLED_Data.h
-../Drivers/OLED/OLED_Data.h:
--- 8_PID/Hardware/Key.h
+++
@@ -1,6 +0,0 @@
-#ifndef __KEY_H_
-#define __KEY_H_
-
-uint8_t KeyNum(void);
-
-#endif
