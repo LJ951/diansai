18913debdd422b1dd97a48a812fe9c7b17cfd4e8
LJ951 <124690250+LJ951@users.noreply.github.com>
2025-07-12 11:00:12 +0800

    Delete 5_mpu6050_V1.1 directory

--- 5_mpu6050_V1.1/Debug/Drivers/MPU6050/inv_mpu.d
+++
@@ -1,8 +0,0 @@
-# FIXED
-
-Drivers/MPU6050/inv_mpu.o: ../Drivers/MPU6050/inv_mpu.c \
- ../Drivers/MPU6050/inv_mpu.h ../Drivers/MPU6050/mspm0_i2c.h \
- C:/Users/22334/workspace_ccstheia/5_mpu6050_V1.1/Drivers/MSPM0/clock.h
-../Drivers/MPU6050/inv_mpu.h:
-../Drivers/MPU6050/mspm0_i2c.h:
-C:/Users/22334/workspace_ccstheia/5_mpu6050_V1.1/Drivers/MSPM0/clock.h:
