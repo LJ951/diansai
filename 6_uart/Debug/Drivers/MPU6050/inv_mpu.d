e032f5aabe7fe7293a689de479d90c3834e53ee6
LJ951 <124690250+LJ951@users.noreply.github.com>
2025-07-12 11:00:35 +0800

    Delete 6_uart directory

--- 6_uart/Debug/Drivers/MPU6050/inv_mpu.d
+++
@@ -1,8 +0,0 @@
-# FIXED
-
-Drivers/MPU6050/inv_mpu.o: ../Drivers/MPU6050/inv_mpu.c \
- ../Drivers/MPU6050/inv_mpu.h ../Drivers/MPU6050/mspm0_i2c.h \
- C:/Users/22334/workspace_ccstheia/6_uart/Drivers/MSPM0/clock.h
-../Drivers/MPU6050/inv_mpu.h:
-../Drivers/MPU6050/mspm0_i2c.h:
-C:/Users/22334/workspace_ccstheia/6_uart/Drivers/MSPM0/clock.h:
