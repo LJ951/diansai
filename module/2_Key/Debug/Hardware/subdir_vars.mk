c0c77efba8ad09c0a88721e05b1320a5c30661bf
LJ951 <124690250+LJ951@users.noreply.github.com>
2025-07-12 11:07:33 +0800

    Add files via upload

---
+++ module/2_Key/Debug/Hardware/subdir_vars.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Hardware/Key.c \
+../Hardware/LED.c 
+
+C_DEPS += \
+./Hardware/Key.d \
+./Hardware/LED.d 
+
+OBJS += \
+./Hardware/Key.o \
+./Hardware/LED.o 
+
+OBJS__QUOTED += \
+"Hardware\Key.o" \
+"Hardware\LED.o" 
+
+C_DEPS__QUOTED += \
+"Hardware\Key.d" \
+"Hardware\LED.d" 
+
+C_SRCS__QUOTED += \
+"../Hardware/Key.c" \
+"../Hardware/LED.c" 
+
+
---
+++ module/2_Key/User/Delay.h
@@ -0,0 +1,6 @@
+#ifndef __DELAY_H_
+#define __DELAY_H_
+
+void Delay_ms(uint32_t ms);
+
+#endif
