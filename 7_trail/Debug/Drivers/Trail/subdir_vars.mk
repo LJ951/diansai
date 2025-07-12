07ba89b5e040795308d11894445177809d26a44c
LJ951 <124690250+LJ951@users.noreply.github.com>
2025-07-12 10:47:19 +0800

    Add files via upload

---
+++ 7_trail/Debug/Drivers/Trail/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/Trail/Trail.c 
+
+C_DEPS += \
+./Drivers/Trail/Trail.d 
+
+OBJS += \
+./Drivers/Trail/Trail.o 
+
+OBJS__QUOTED += \
+"Drivers\Trail\Trail.o" 
+
+C_DEPS__QUOTED += \
+"Drivers\Trail\Trail.d" 
+
+C_SRCS__QUOTED += \
+"../Drivers/Trail/Trail.c" 
+
+
---
+++ 7_trail/Debug/Drivers/UART/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/UART/Serial.c 
+
+C_DEPS += \
+./Drivers/UART/Serial.d 
+
+OBJS += \
+./Drivers/UART/Serial.o 
+
+OBJS__QUOTED += \
+"Drivers\UART\Serial.o" 
+
+C_DEPS__QUOTED += \
+"Drivers\UART\Serial.d" 
+
+C_SRCS__QUOTED += \
+"../Drivers/UART/Serial.c" 
+
+
