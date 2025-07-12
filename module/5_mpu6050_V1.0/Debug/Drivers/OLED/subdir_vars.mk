afb12e149b6d72945c0d577a3813cd53ed79f43c
LJ951 <124690250+LJ951@users.noreply.github.com>
2025-07-12 11:12:41 +0800

    Add files via upload

---
+++ module/5_mpu6050_V1.0/Debug/Drivers/OLED/subdir_vars.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/OLED/OLED.c \
+../Drivers/OLED/OLED_Data.c 
+
+C_DEPS += \
+./Drivers/OLED/OLED.d \
+./Drivers/OLED/OLED_Data.d 
+
+OBJS += \
+./Drivers/OLED/OLED.o \
+./Drivers/OLED/OLED_Data.o 
+
+OBJS__QUOTED += \
+"Drivers\OLED\OLED.o" \
+"Drivers\OLED\OLED_Data.o" 
+
+C_DEPS__QUOTED += \
+"Drivers\OLED\OLED.d" \
+"Drivers\OLED\OLED_Data.d" 
+
+C_SRCS__QUOTED += \
+"../Drivers/OLED/OLED.c" \
+"../Drivers/OLED/OLED_Data.c" 
+
+
